4595e62f3287dec90dceff22eadb54ecf6c7c6c2
Saddam Bilalov <saddam.bilalov@outlook.com>
2019-09-01 11:06:48 +0200

    Chnaged EC2 AMI because of the new region.

--- code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
+  ami                    = "ami-0862aabda3fb488b5"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
